abd1b5d7e2b8c5e7d69cce921c37a538276fbb9d
araujonathalia <araujo.nathalia03@gmail.com>
2019-11-19 11:54:24 -0800

    updated GoogleMap

--- frontend-test/my-app/src/Components/GoogleMap/MapWrapper.js
+++ frontend-test/my-app/src/Components/GoogleMap/MapWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const MapWrapper = styled.div`
   position: flex;
   width: 100%;
-  height: calc(100vh - 56px -  32px - 40px - 24px - 24px - 24px); // Navbar bar - padding X2 top/buttom - searchbar - padding - surfacePadding x2
+  height: 100%; 
   min-height: 500px;
 `;
 
--- frontend-test/my-app/src/Components/GoogleMap/index.js
+++ frontend-test/my-app/src/Components/GoogleMap/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import supercluster from 'points-cluster';
-
 import Marker from '../Marker';
 import ClusterMarker from '../ClusterMarker';
-
 import mapStyles from './mapStyles.json';
-import { markersData, center } from '../../fakeData';
-
+import { markersData, center } from '../../preData';
 import MapWrapper from './MapWrapper';
-import Surface from "@hig/surface";
-import Tooltip from "@hig/tooltip";
+import {Card} from "@material-ui/core";
+import CardContent from "@material-ui/core/CardContent";
 
 const MAP = {
   defaultZoom: 0,
-  defaultCenter: center,
+  defaultCenter: {lat: 13.149396, lng: 10.000353},
   options: {
     styles: mapStyles,
     minZoom:2,
@@ -23,8 +20,6 @@ const MAP = {
 };
 
 export class GoogleMap extends React.PureComponent {
-//   eslint-disable-line react/prefer-stateless-function
-
   constructor(props) {
     super(props)
     this.state = {
@@ -33,39 +28,13 @@ export class GoogleMap extends React.PureComponent {
         zoom: MAP.defaultZoom,
       },
       clusters: [],
-      geoData: []
     };
     this.getClusters = this.getClusters.bind(this);
-  }
-
-
-  getClusters = () => {
-    let pings = markersData;
-
-    if(this.state.geoData !== undefined) {
-      pings = this.state.geoData;
-
-    }
-    const clusters = supercluster(pings, {
-      minZoom: 0,
-      maxZoom: 19,
-      radius: 60,
-    });
-
-    return clusters(this.state.mapOptions);
-  };
-  componentDidMount() {
-    this.setState({geoData: this.props.geoData});
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-      if (prevState.geoData != this.props.geoData) {
-        this.setState({geoData: this.props.geoData});
-        this.handleMapChange(this.state.mapOptions)
-      }
+    this.getClusters=this.getClusters.bind(this);
   }
 
   matchNameToCoord(coord) {
-    for (let item of this.state.geoData) {
+    for (let item of this.props.markersData) {
       if (item.lat === coord.lat && item.lng === coord.lng) {
         return item.name;
       }
@@ -75,7 +44,7 @@ export class GoogleMap extends React.PureComponent {
 
   matchNamesToCoord(coord) {
     let names = [];
-    for (let item of this.state.geoData) {
+    for (let item of this.props.markersData) {
       if (item.lat === coord.lat && item.lng === coord.lng) {
         names.push(item.name);
       }
@@ -83,17 +52,28 @@ export class GoogleMap extends React.PureComponent {
     return names;
   }
 
+  getClusters = () => {
+    console.log("calling ...");
+    console.log(this.props.markersData);
+    const clusters = supercluster(this.props.markersData, {
+      maxZoom: 19,
+      radius: 60,
+    });
+
+    return clusters(this.state.mapOptions);
+  };
+
   createClusters = props => {
     this.setState({
       clusters: this.state.mapOptions.bounds
-        ? this.getClusters(props).map(({ wx, wy, numPoints, points }) => ({
+          ? this.getClusters(props).map(({ wx, wy, numPoints, points }) => ({
             lat: wy,
             lng: wx,
             numPoints,
             id: `${numPoints}_${points[0].id}`,
             points,
           }))
-        : [],
+          : [],
     });
   };
 
@@ -109,58 +89,62 @@ export class GoogleMap extends React.PureComponent {
       () => {
         this.createClusters(this.props);
         this.matchNamesToCoord=this.matchNamesToCoord.bind(this);
-        this.matchNameToCoord=this.matchNameToCoord.bind(this);
-      }
+        this.matchNameToCoord=this.matchNameToCoord.bind(this);      }
     );
   };
 
   render() {
     return (
-      <div
-          horizontalPadding="s"
-          verticalPadding="s"
-          shadow="low"
-          borderRadius="s"
+      <Card
+        syle={{
+          padding: "10px",
+          borderRadius: "15px",
+          minHeight: "auto",
+          height: "100%",
+          width: "100%"
+        }}
       >
-        <MapWrapper>
-          <GoogleMapReact
-              key={"map"}
-            defaultZoom={MAP.defaultZoom}
-            defaultCenter={MAP.defaultCenter}
-            options={MAP.options}
-            onChange={this.handleMapChange}
-            yesIWantToUseGoogleMapApiInternals
-            bootstrapURLKeys={{ key: 'AIzaSyBAVa1tMJT6RuCDAiyYZGCVTFTE7VZXuaM ' }}
-          >
-            {this.state.clusters.map(item => {
-              if (item.numPoints === 1) {
+        <CardContent style={{height: "580px", backgroundColor:"#333a42"}}>
+          <MapWrapper>
+            <GoogleMapReact
+                key={"map"}
+                defaultZoom={MAP.defaultZoom}
+                defaultCenter={MAP.defaultCenter}
+                options={MAP.options}
+                onChange={this.handleMapChange}
+                yesIWantToUseGoogleMapApiInternals
+                bootstrapURLKeys={{ key: 'AIzaSyBAVa1tMJT6RuCDAiyYZGCVTFTE7VZXuaM' }}
+            >
+              {this.state.clusters.map(item => {
+                if (item.numPoints === 1) {
+                  return (
+                      <Marker
+                          key={`${item.id}${item.points[0].lng + item.points[0].lat}`}
+                          lat={item.points[0].lat}
+                          lng={item.points[0].lng}
+                          names={this.matchNameToCoord({lat: item.points[0].lat, lng: item.points[0].lng})}
+                          search={this.props.search}
+
+                      >
+
+                      </Marker>
+                  );
+                }
+
                 return (
-                  <Marker
-                    key={`${item.id}${item.points[0].lng + item.points[0].lat}`}
-                    lat={item.points[0].lat}
-                    lng={item.points[0].lng}
-                    names={this.matchNameToCoord({lat: item.points[0].lat, lng: item.points[0].lng})}
-                  >
-
-                  </Marker>
+                    <ClusterMarker
+                        key={item.id}
+                        lat={item.lat}
+                        lng={item.lng}
+                        points={item.points}
+                        matchNamesToCoord={this.matchNamesToCoord}
+                    />
                 );
-              }
-
-              return (
-
-                <ClusterMarker
-                  key={item.id}
-                  lat={item.lat}
-                  lng={item.lng}
-                  points={item.points}
-                  matchNamesToCoord={this.matchNamesToCoord}
-                />
-
-              );
-            })}
-          </GoogleMapReact>
-        </MapWrapper>
-      </div>
+              })}
+            </GoogleMapReact>
+          </MapWrapper>
+        </CardContent>
+      </Card>
     );
   }
 }
