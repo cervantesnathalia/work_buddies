7a1d528cfd24461b227054d9455fbf45ef100820
araujonathalia <araujo.nathalia03@gmail.com>
2019-11-19 11:55:46 -0800

    updated Marker

--- frontend-test/my-app/src/Components/Marker/index.js
+++ frontend-test/my-app/src/Components/Marker/index.js
@@ -1,56 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import MarkerStyled from './MarkerStyled';
 import MarkerInGroupStyled from './MarkerInGroupStyled';
 import Ping from '../Ping';
-import Tooltip from '@hig/tooltip';
-import Typography from '@hig/typography';
-import { Tooltip } from 'antd';
+import { Tooltip } from '@material-ui/core';
+import Typography from "@material-ui/core/Typography";
+
 
 class Marker extends React.PureComponent {
-//   eslint-disable-line react/prefer-stateless-function
   static defaultProps = {
     inGroup: false,
     isOpen: false
   };
 
-
   render() {
-
     return (
       <div>
 
-        {this.props.inGroup
-
-          ?
+        {this.props.inGroup ?
             <Tooltip
                 key={`${this.props.names}`}
-                content={<span className="css-1x6j4q8" style={{color: "rgb(60, 60, 60)",fontSize: "12px"}}>{this.props.names.map((name, index) => {
-                  return (
-                      <div key={`${name}${index}`}>{name}</div>
-                  )
-                })}</span>}
-                anchorPoint="bottom-center"
-                openOnHover
-            >
-              <MarkerInGroupStyled
-
-              >
-                  <Ping scale="0.55" />
+                title={<Typography>
+                  {this.props.names.map((name, index) => {
+                    return (
+                        <div key={`${name}${index}`}>{name}</div>
+                  )})}
+                  </Typography>
+                }
+                placement="top">
+              <MarkerInGroupStyled>
+                <Ping scale="0.55" />
               </MarkerInGroupStyled>
             </Tooltip>
+
           :
             <Tooltip
                 key={`${this.props.names}`}
-                content={<span className="css-1x6j4q8" style={{color: "rgb(60, 60, 60)",fontSize: "12px"}}>{this.props.names}</span>}
-                anchorPoint="bottom-center"
-                openOnHover
+                title={<Typography>{this.props.names}</Typography>}
+                placement="top"
             >
               <MarkerStyled
-
+                  onClick={() => {this.props.search(this.props.names, "Individual")}}
               >
-              <Ping scale="0.55" />
+                <Ping scale="0.55" />
               </MarkerStyled>
             </Tooltip>
            }
