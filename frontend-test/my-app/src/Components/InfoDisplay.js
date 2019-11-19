c02b0c7b2ac140f774f569c8d9d0547e9f59308e
araujonathalia <araujo.nathalia03@gmail.com>
2019-11-19 13:21:16 -0800

    updated - InfDisplay

--- frontend-test/my-app/src/Components/InfoDisplay.js
+++ frontend-test/my-app/src/Components/InfoDisplay.js
@@ -1,85 +1,44 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, {Component} from 'react';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-const useStyles = makeStyles({
-    card: {
-        minWidth: "550px",
-        minHeight: "400px",
-        padding: "10px",
-        borderRadius: "15px",
-        height: "auto",
-        width: "100%"
+import NoData from "./displays/NoData";
+import AllUsers from "./displays/AllUsers";
+import User from "./displays/User";
+import AllTeams from "./displays/AllTeams";
+
+class InfoDisplay extends Component {
+    pickDisplay(data, type, props=undefined) {
+        switch (type) {
+            case "Individual":
+                if (data.length > 1) return (<AllUsers data={data} search={this.props.search}/>);
+                return (<User {...props}/>);
+            case "Team":
+                return (<AllTeams data={data}/>);
+            default:
+                return (<NoData/>);
+        }
     }
-});
-export default function InfoDisplay(props) {
-    const classes = useStyles();
-    const { data, stype } = props;
-    console.log(data);
-    if (data !== undefined) {
-        console.log(stype)
-        if (stype === "Individual") {   
-            if (data.length > 1) {
-                return (
-                    <div style={{ margin: "20px 25px 20px 25px" }}>
-                        <Card className={classes.card}>
-                            <CardContent>
-                                {data.map((value, index) => {
-                                    return (
-                                        <div key={index}>
-                                            <h1>{value.name}</h1>
-                                        </div>
-                                    );
-                                })}
-                            </CardContent>
-                        </Card>
-                    </div>
-                );
-            } else {
-                return (
-                <div style={{ margin: "20px 25px 20px 25px" }}>
-                    <Card className={classes.card}>
-                        <CardContent>
-                            {data.map((value, index) => {
-                                return (
-                                    <div key={index}>
-                                        <h1>{value.name}</h1>
-                                        <p><b>Office Location: </b>{value.office_location}</p>
-                                        <p><b>Business title: </b> {value.business_title}</p>
-                                        <p><b>Slack Name: </b> {value.slack_name}</p>
-                                        <p><b>Email: </b> {value.work_email}</p>
-                                    </div>
-                                );
-                            })}
-                        </CardContent>
-                    </Card>
-                </div>
-            );
-            }   
-            
-                        // }
-        } else {
-            return (
-                <div style={{ margin: "20px 25px 20px 25px" }}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        {data.map((value, index) => {
-                            return <p key={index}>{value.team_name}</p>;
-                        })}
+
+    render() {
+        console.log(this.props);
+        const { searchResult, type } = this.props;
+        return (
+                <Card
+                    syle={{
+                        padding: "10px",
+                        borderRadius: "15px",
+                        minHeight: "auto",
+                        height: "100%",
+                        width: "100%",
+                        backgroundColor:"#333a42"
+                    }}
+                >
+                    <CardContent style={{height: "580px", backgroundColor:"#333a42", color:"#fff"}}>
+                        {this.pickDisplay(searchResult, type, this.props)}
                     </CardContent>
                 </Card>
-            </div>
-            ); 
-        }
-        
-    } else {
-        return (
-            <div style={{ margin: "20px 25px 20px 25px" }}>
-                <Card className={classes.card}>
-                    <CardContent>{"No Data"}</CardContent>
-                </Card>
-            </div>
         );
     }
 }
+
+export default InfoDisplay;
